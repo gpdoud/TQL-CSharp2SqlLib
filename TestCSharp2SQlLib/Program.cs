81523a711fad85a704a432d1234e2a044896c462
Greg Doud <gpdoud@gmail.com>
2021-05-25 16:36:19 -0400

    Add project files.

---
+++ TestCSharp2SQlLib/Program.cs
@@ -0,0 +1,19 @@
+﻿using CSharp2SqlLib;
+
+using System;
+
+namespace TestCSharp2SQlLib {
+    class Program {
+        static void Main(string[] args) {
+
+            var sqllib = new SqlLib();
+            sqllib.Connect();
+
+            var users = sqllib.GetAllUsers();
+            var user = sqllib.GetByPK(1);
+            var nulluser = sqllib.GetByPK(0);
+
+            sqllib.Disconnect();
+        }
+    }
+}
